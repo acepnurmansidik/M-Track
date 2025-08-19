893c6d72a44cfb25563357764f329355073439be
acep nurman sidik <acepnurmansidik@gmail.com>
2025-08-19 11:13:11 +0700

    make clean code

--- lib/pages/dashboard/detail_category_page.dart
+++ lib/pages/dashboard/detail_category_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers, unused_element
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracking/models/transaction_model.dart';
@@ -62,235 +64,8 @@ class _DetailCategoryPageState extends State<DetailCategoryPage> {
     final chartData = getChartData();
     labelSelected = chartData[selectedIndex].periode;
 
-    Widget appBarSection() {
-      return AppBar(
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        backgroundColor: kBaseColors,
-        surfaceTintColor: kBaseColors,
-        title: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
-          child: Row(
-            children: [
-              GestureDetector(
-                onTap: () => Navigator.pop(context),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const Icon(Icons.chevron_left, color: Colors.black),
-                ),
-              ),
-              Expanded(
-                // <-- Biarkan teks mengambil sisa ruang
-                child: Text(
-                  toTitleCase(widget.title),
-                  textAlign: TextAlign.center, // <-- Pusatkan teks
-                  style: blackTextStyle.copyWith(fontSize: 18),
-                ),
-              ),
-              const SizedBox(
-                width: 40,
-                height: 40,
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
-    Widget titleSection({String title = "", int nominal = 0}) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 21),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text.rich(
-              TextSpan(children: [
-                const TextSpan(text: "Spending · "),
-                TextSpan(text: labelSelected),
-              ]),
-              style: greyTextStyle.copyWith(fontSize: 14),
-            ),
-            const SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  formatRupiah(nominal),
-                  style: blackTextStyle.copyWith(
-                      fontSize: 20, fontWeight: semibold),
-                ),
-              ],
-            )
-          ],
-        ),
-      );
-    }
-
-    Widget barChartSection() {
-      return SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Container(
-          margin: const EdgeInsets.only(bottom: 20),
-          height: 280,
-          width:
-              (getChartData().length * 65) + 100, // 50px per batang + padding
-          child: SfCartesianChart(
-            margin: const EdgeInsets.all(0),
-            plotAreaBorderWidth: 0,
-            primaryXAxis: const CategoryAxis(
-              majorGridLines: MajorGridLines(width: 0),
-              axisLine: AxisLine(width: 0), // Hilangkan garis axis horizontal
-              majorTickLines: MajorTickLines(size: 0), // Hilangkan tick marks
-            ),
-            primaryYAxis: const NumericAxis(
-              isVisible: false,
-              interval: 5,
-              majorGridLines:
-                  MajorGridLines(width: 0), // Hilangkan grid horizontal
-              axisLine: AxisLine(width: 0), // Hilangkan garis axis
-              majorTickLines: MajorTickLines(size: 0),
-            ),
-            series: <CartesianSeries>[
-              ColumnSeries<ChartData, String>(
-                width: .65,
-                // spacing: .1,
-                dataSource: getChartData(),
-                xValueMapper: (ChartData chart, _) => chart.periode,
-                yValueMapper: (ChartData chart, _) => chart.nominal,
-                dataLabelSettings: const DataLabelSettings(
-                  isVisible: false,
-                  textStyle: TextStyle(color: Colors.black),
-                ),
-                borderRadius: BorderRadius.circular(7),
-
-                color: Colors.grey[200], // Warna default abu-abu
-                onPointTap: (pointInteractionDetails) {
-                  setState(() {
-                    selectedIndex = pointInteractionDetails.pointIndex!;
-                  });
-                },
-                pointColorMapper: (ChartData data, int index) {
-                  return index == selectedIndex
-                      ? kPrimaryV2Color
-                      : Colors.grey[200]; // Warna abu-abu default
-                },
-              )
-            ],
-          ),
-        ),
-      );
-    }
-
-    Widget cashflowSection() {
-      Widget cashflowItem(bool isIncome, String title, int amount) {
-        return SizedBox(
-          width: MediaQuery.of(context).size.width / 2.23,
-          height: 60,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 40,
-                width: 40,
-                margin: const EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: isIncome ? Colors.green[50] : Colors.red[50],
-                ),
-                child: Transform.rotate(
-                  angle: isIncome ? 1.6 : -1.6,
-                  child: Icon(
-                    Icons.arrow_back,
-                    size: 20,
-                    color: isIncome ? Colors.green[500] : Colors.red[500],
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    formatRupiah(amount),
-                    style: greenTextStyle.copyWith(
-                      color: isIncome ? Colors.green[300] : Colors.red[300],
-                    ),
-                  ),
-                  Text(
-                    title,
-                    style: greyTextStyle,
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      }
-
-      return SizedBox(
-        height: 60,
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            cashflowItem(true, "Income", dataSelected.income),
-            cashflowItem(false, "Spending", dataSelected.expense),
-          ],
-        ),
-      );
-    }
-
-    Widget transactionListSection() {
-      return SizedBox(
-        height: dataSelected.listData.isNotEmpty ? 250 : 150,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: dataSelected.listData.isNotEmpty
-                ? dataSelected.listData.map((everyItem) {
-                    return TransactionItem(
-                      title: everyItem.categoryName,
-                      datetime: everyItem.date,
-                      nominal: everyItem.totalAmount,
-                      isIncome: everyItem.typeName,
-                    );
-                  }).toList()
-                : [
-                    SizedBox(
-                      height: 150,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 115,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('assets/empty-box.png'),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            "Aww... there's not transaction",
-                            style: blackTextStyle.copyWith(fontWeight: medium),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size(double.infinity, 60),
-          child: appBarSection()),
+      appBar: _buildAppBar(),
       backgroundColor: kBaseColors,
       body: BlocBuilder<TransactionCubit, TransactionState>(
         builder: (context, state) {
@@ -302,62 +77,252 @@ class _DetailCategoryPageState extends State<DetailCategoryPage> {
                 state.transactionPeriode.data[0].listData.map((everyItem) {
               return ChartData(everyItem.date, everyItem.totalAmount);
             }).toList();
-            return ListView(
-              padding: EdgeInsets.only(
-                left: defaultMargin,
-                right: defaultMargin,
-                bottom: 20,
-              ),
-              children: [
-                titleSection(
-                  title: dataSelected.periode,
-                  nominal: dataSelected.totalCategory,
-                ),
-                barChartSection(),
-                cashflowSection(),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Last transaction',
-                        style: greyTextStyle.copyWith(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ),
-                transactionListSection(),
-              ],
-            );
+            return _buildContent();
           }
-          return ListView(
-            padding: EdgeInsets.only(
-              left: defaultMargin,
-              right: defaultMargin,
-              bottom: 20,
-            ),
-            children: [
-              titleSection(),
-              barChartSection(),
-              cashflowSection(),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'My Pocket',
-                      style: greyTextStyle.copyWith(fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-              transactionListSection(),
-            ],
-          );
+          return _buildEmptyState();
         },
       ),
+    );
+  }
+
+  Widget _buildContent() {
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+      children: [
+        _buildTitleSection(),
+        _buildBarChartSection(),
+        _buildCashflowSection(),
+        _buildTransactionListSection(),
+      ],
+    );
+  }
+
+  Widget _buildTransactionListSection() {
+    List<Widget> _buildEmptyTransactionMessage() {
+      return [
+        SizedBox(
+          height: 150,
+          child: Column(
+            children: [
+              Container(
+                height: 115,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/empty-box.png'),
+                  ),
+                ),
+              ),
+              Text(
+                "Aww... there's no transaction",
+                style: blackTextStyle.copyWith(fontWeight: medium),
+              ),
+            ],
+          ),
+        ),
+      ];
+    }
+
+    return SizedBox(
+      height: dataSelected.listData.isNotEmpty ? 250 : 150,
+      child: SingleChildScrollView(
+        child: Column(
+          children: dataSelected.listData.isNotEmpty
+              ? dataSelected.listData.map((everyItem) {
+                  return TransactionItem(
+                    title: everyItem.categoryName,
+                    datetime: everyItem.date,
+                    nominal: everyItem.totalAmount,
+                    isIncome: everyItem.typeName,
+                  );
+                }).toList()
+              : _buildEmptyTransactionMessage(),
+        ),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      backgroundColor: kBaseColors,
+      title: Row(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(Icons.chevron_left, color: Colors.black),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              toTitleCase(widget.title),
+              textAlign: TextAlign.center,
+              style: blackTextStyle.copyWith(fontSize: 18),
+            ),
+          ),
+          const SizedBox(width: 40, height: 40),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitleSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 21),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text.rich(
+            TextSpan(children: [
+              const TextSpan(text: "Spending · "),
+              TextSpan(text: labelSelected),
+            ]),
+            style: greyTextStyle.copyWith(fontSize: 14),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            formatRupiah(dataSelected.totalCategory),
+            style: blackTextStyle.copyWith(fontSize: 20, fontWeight: semibold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCashflowSection() {
+    Widget _buildCashflowItem(bool isIncome, String title, int amount) {
+      return SizedBox(
+        width: MediaQuery.of(context).size.width / 2.23,
+        height: 60,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              margin: const EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isIncome ? Colors.green[50] : Colors.red[50],
+              ),
+              child: Transform.rotate(
+                angle: isIncome ? 1.6 : -1.6,
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 20,
+                  color: isIncome ? Colors.green[500] : Colors.red[500],
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  formatRupiah(amount),
+                  style: greenTextStyle.copyWith(
+                    color: isIncome ? Colors.green[300] : Colors.red[300],
+                  ),
+                ),
+                Text(title, style: greyTextStyle),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildCashflowItem(true, "Income", dataSelected.income),
+          _buildCashflowItem(false, "Spending", dataSelected.expense),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBarChartSection() {
+    final chartData = getChartData();
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        height: 280,
+        width: (chartData.length * 65) + 100,
+        child: SfCartesianChart(
+          margin: const EdgeInsets.all(0),
+          plotAreaBorderWidth: 0,
+          primaryXAxis: const CategoryAxis(
+            majorGridLines: MajorGridLines(width: 0),
+            axisLine: AxisLine(width: 0),
+            majorTickLines: MajorTickLines(size: 0),
+          ),
+          primaryYAxis: const NumericAxis(
+            isVisible: false,
+            interval: 5,
+            majorGridLines: MajorGridLines(width: 0),
+            axisLine: AxisLine(width: 0),
+            majorTickLines: MajorTickLines(size: 0),
+          ),
+          series: <CartesianSeries>[
+            ColumnSeries<ChartData, String>(
+              width: .65,
+              dataSource: chartData,
+              xValueMapper: (ChartData chart, _) => chart.periode,
+              yValueMapper: (ChartData chart, _) => chart.nominal,
+              dataLabelSettings: const DataLabelSettings(isVisible: false),
+              borderRadius: BorderRadius.circular(7),
+              color: Colors.grey[200],
+              onPointTap: (pointInteractionDetails) {
+                setState(() {
+                  selectedIndex = pointInteractionDetails.pointIndex!;
+                });
+              },
+              pointColorMapper: (ChartData data, int index) {
+                return index == selectedIndex
+                    ? kPrimaryV2Color
+                    : Colors.grey[200];
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return ListView(
+      padding: EdgeInsets.only(
+        left: defaultMargin,
+        right: defaultMargin,
+        bottom: 20,
+      ),
+      children: [
+        _buildTitleSection(),
+        _buildBarChartSection(),
+        _buildCashflowSection(),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'My Pocket',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+        _buildTransactionListSection(),
+      ],
     );
   }
 }
