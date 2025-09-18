153ab93a837594afa6a182f93578bdd38f49174d
acep nurman sidik <acepnurmansidik@gmail.com>
2025-09-18 10:34:31 +0700

    update: added naimation

--- lib/pages/success_page.dart
+++ lib/pages/success_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 import 'package:tracking/cubit/page_cubit.dart';
 import 'package:tracking/theme.dart';
 import 'package:tracking/widgets/custom_button.dart';
@@ -10,45 +11,60 @@ class SuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBaseColors,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            Container(
-              width: 200,
-              height: 200,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/saving-money.png"),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/saving-money.png"),
+                    ),
+                  ),
                 ),
+                Text(
+                  "Yeay, you did it 😍",
+                  style: blackTextStyle.copyWith(
+                    fontSize: 28,
+                    fontWeight: semibold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "Come on, record your money savings",
+                  style:
+                      blackTextStyle.copyWith(fontSize: 16, fontWeight: light),
+                ),
+                CustomButton(
+                  title: 'Back to menu',
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 50),
+                  widthBtn: 220,
+                  onPressed: () {
+                    context.read<PageCubit>().setPage(0);
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      '/main',
+                      (route) => false,
+                    );
+                  },
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Lottie.asset(
+                "assets/animations/Confetti.json",
+                repeat: false,
+                width: MediaQuery.of(context).size.width,
               ),
-            ),
-            Text(
-              "Yeay, you did it 😍",
-              style: blackTextStyle.copyWith(
-                fontSize: 28,
-                fontWeight: semibold,
-              ),
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Text(
-              "Come on, record your money savings",
-              style: blackTextStyle.copyWith(fontSize: 16, fontWeight: light),
-            ),
-            CustomButton(
-              title: 'Back to menu',
-              margin: const EdgeInsets.only(left: 20, right: 20, top: 50),
-              widthBtn: 220,
-              onPressed: () {
-                context.read<PageCubit>().setPage(0);
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  '/main',
-                  (route) => false,
-                );
-              },
             )
           ],
         ),
